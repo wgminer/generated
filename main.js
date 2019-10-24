587bd93310ab74d6e3e3a1153e883f0cf18c4915
William Miner <wgminer@gmail.com>
2019-10-24 11:41:46 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,385 @@
+const palettes = [
+    {
+        "liked": false,
+        "id": 2252,
+        "name": "The Deep Blue",
+        "slug": "the-deep-blue",
+        "likes": 602,
+        "colors": ["#7ec8e3", "#0000ff", "#000c66", "#050a30"]
+    }, 
+    {
+        "liked": false,
+        "id": 1,
+        "name": "Up Shore",
+        "slug": "up-shore",
+        "likes": 589,
+        "colors": ["#0292b7", "#1ac8db", "#8c756a", "#dee2ec"]
+    }, 
+    {
+        "liked": false,
+        "id": 759,
+        "name": "Rosettes and Cream",
+        "slug": "rosettes-and-cream",
+        "likes": 522,
+        "colors": ["#d8a7b1", "#b6e2d3", "#fae8e0", "#ef7c8e"]
+    }, 
+    {
+        "liked": false,
+        "id": 99,
+        "name": "Luscious Lemonade",
+        "slug": "luscious-lemonade",
+        "likes": 509,
+        "colors": ["#e1c340", "#4cd7d0", "#a4e8e0", "#f8ea8c"]
+    }, 
+    {
+        "liked": false,
+        "id": 3290,
+        "name": "Room for Comfort",
+        "slug": "room-for-comfort",
+        "likes": 419,
+        "colors": ["#eeede7", "#868b8e", "#b9b7bd", "#e7d2cc"]
+    }, 
+    {
+        "liked": false,
+        "id": 869,
+        "name": "Mermaid Lagoon",
+        "slug": "mermaid-lagoon",
+        "likes": 401,
+        "colors": ["#b1d4e0", "#2e8bc0", "#0c2d48", "#145da0"]
+    }, 
+    {
+        "liked": false,
+        "id": 1961,
+        "name": "Healthy Leaves",
+        "slug": "healthy-leaves",
+        "likes": 370,
+        "colors": ["#59981a", "#ecf87f", "#81b622", "#3d550c"]
+    }, 
+    {
+        "liked": false,
+        "id": 1120,
+        "name": "Berry Blues",
+        "slug": "berry-blues",
+        "likes": 363,
+        "colors": ["#4e1a3d", "#cfeed1", "#ff2768", "#05e0e9"]
+    }, 
+    {
+        "liked": false,
+        "id": 1183,
+        "name": "Window Tide",
+        "slug": "window-tide",
+        "likes": 361,
+        "colors": ["#c3e0e5", "#274472", "#5885af", "#41729f"]
+    }, 
+    {
+        "liked": false,
+        "id": 2584,
+        "name": "Emerald Entrance",
+        "slug": "emerald-entrance",
+        "likes": 353,
+        "colors": ["#b68d40", "#f4ebd0", "#122620", "#d6ad60"]
+    }, 
+    {
+        "liked": false,
+        "id": 88,
+        "name": "Retro Punch",
+        "slug": "retro-punch",
+        "likes": 343,
+        "colors": ["#f51720", "#fa26a0", "#f8d210", "#2ff3e0"]
+    }, 
+    {
+        "liked": false,
+        "id": 619,
+        "name": "Salmon Sushi",
+        "slug": "salmon-sushi",
+        "likes": 337,
+        "colors": ["#e7f2f8", "#74bdcb", "#ffa384", "#efe7bc"]
+    }, 
+    {
+        "liked": false,
+        "id": 614,
+        "name": "Foil-covered Sneakers",
+        "slug": "foil-covered-sneakers",
+        "likes": 330,
+        "colors": ["#333652", "#fad02c", "#e9eaec", "#90adc6"]
+    }, 
+    {
+        "liked": false,
+        "id": 1297,
+        "name": "Rosy Dew",
+        "slug": "rosy-dew",
+        "likes": 322,
+        "colors": ["#f9f1f0", "#fadcd9", "#f8afa6", "#f79489"]
+    }, 
+    {
+        "liked": false,
+        "id": 101,
+        "name": "Diaphanous Jellyfish",
+        "slug": "diaphanous-jellyfish",
+        "likes": 313,
+        "colors": ["#041f60", "#0476d0", "#2ceef0", "#b4f5f0"]
+    }, 
+    {
+        "liked": false,
+        "id": 655,
+        "name": "Wild Orchid",
+        "slug": "wild-orchid",
+        "likes": 309,
+        "colors": ["#741aac", "#340744", "#debad6", "#005437"]
+    }, 
+    {
+        "liked": false,
+        "id": 1727,
+        "name": "Soft Focus Forest",
+        "slug": "soft-focus-forest",
+        "likes": 293,
+        "colors": ["#0c1446", "#87aca3", "#2b7c85", "#175873"]
+    }, 
+    {
+        "liked": false,
+        "id": 393,
+        "name": "Petal Party",
+        "slug": "petal-party",
+        "likes": 292,
+        "colors": ["#870a30", "#ec8fd0", "#d43790", "#f2c5e0"]
+    }, 
+    {
+        "liked": false,
+        "id": 1718,
+        "name": "In the Blue",
+        "slug": "in-the-blue",
+        "likes": 288,
+        "colors": ["#0c6980", "#00a8a8", "#2eb5e0", "#c4dbe0"]
+    }, 
+    {
+        "liked": false,
+        "id": 691,
+        "name": "Pasture of Dreams",
+        "slug": "pasture-of-dreams",
+        "likes": 281,
+        "colors": ["#613659", "#211522", "#c197d2", "#d3b1c2"]
+    }, 
+    {
+        "liked": false,
+        "id": 508,
+        "name": "Berry Jar",
+        "slug": "berry-jar",
+        "likes": 271,
+        "colors": ["#f83839", "#513b41", "#7fe5f0", "#c8f4f9"]
+    }, 
+    {
+        "liked": false,
+        "id": 596,
+        "name": "Mellow Barnyard",
+        "slug": "mellow-barnyard",
+        "likes": 264,
+        "colors": ["#282120", "#fad02c", "#f8efe4", "#e6c2bf"]
+    }, 
+    {
+        "liked": false,
+        "id": 128,
+        "name": "Icy Gradient",
+        "slug": "icy-gradient",
+        "likes": 262,
+        "colors": ["#eeb5eb", "#c26dbc", "#c8f4f9", "#3cacae"]
+    }, 
+    {
+        "liked": false,
+        "id": 132,
+        "name": "Bamboo Beach",
+        "slug": "bamboo-beach",
+        "likes": 261,
+        "colors": ["#5d6c89", "#36d6e7", "#feb06a", "#fbf6f3"]
+    }, 
+    {
+        "liked": false,
+        "id": 925,
+        "name": "Melted Ice Cream",
+        "slug": "melted-ice-cream",
+        "likes": 259,
+        "colors": ["#f6c8cc", "#ebf5f7", "#c15b78", "#5bb0ba"]
+    }, 
+    {
+        "liked": false,
+        "id": 102,
+        "name": "Sea of Serenity",
+        "slug": "sea-of-serenity",
+        "likes": 250,
+        "colors": ["#059dc0", "#6af2f0", "#04d4f0", "#04ecf0"]
+    }, 
+    {
+        "liked": false,
+        "id": 63,
+        "name": "Athleisure Style",
+        "slug": "athleisure-style",
+        "likes": 244,
+        "colors": ["#ce2380", "#d6dfe0", "#171810", "#07dac0"]
+    }, 
+    {
+        "liked": false,
+        "id": 2526,
+        "name": "Summer Splash",
+        "slug": "summer-splash",
+        "likes": 236,
+        "colors": ["#d4f1f4", "#75e6da", "#189ab4", "#05445e"]
+    }, 
+    {
+        "liked": false,
+        "id": 2892,
+        "name": "Way up North",
+        "slug": "way-up-north",
+        "likes": 231,
+        "colors": ["#1e3d58", "#057dcd", "#43b0f1", "#e8eef1"]
+    }, 
+    {
+        "liked": false,
+        "id": 3209,
+        "name": "Watermelon and Stripes",
+        "slug": "watermelon-and-stripes",
+        "likes": 224,
+        "colors": ["#1d741b", "#88ca5e", "#f1c0b9", "#d2fba4"]
+    }, 
+    {
+        "liked": false,
+        "id": 898,
+        "name": "Blushing Landscape",
+        "slug": "blushing-landscape",
+        "likes": 222,
+        "colors": ["#ff8882", "#543855", "#e88d72", "#ffa384"]
+    }, 
+    {
+        "liked": false,
+        "id": 1425,
+        "name": "Pastel Dreams",
+        "slug": "pastel-dreams",
+        "likes": 214,
+        "colors": ["#ffaebc", "#a0e7e5", "#b4f8c8", "#fbe7c6"]
+    }, 
+    {
+        "liked": false,
+        "id": 599,
+        "name": "Pomegranate on Print",
+        "slug": "pomegranate-on-print",
+        "likes": 212,
+        "colors": ["#efebe2", "#b175ff", "#ab0552", "#07040a"]
+    }, 
+    {
+        "liked": false,
+        "id": 576,
+        "name": "Green Tracks",
+        "slug": "green-tracks",
+        "likes": 212,
+        "colors": ["#43b14b", "#e6d7d5", "#9e6f6d", "#1e3148"]
+    }, 
+    {
+        "liked": false,
+        "id": 901,
+        "name": "Northern Lights",
+        "slug": "northern-lights-3",
+        "likes": 211,
+        "colors": ["#eaeae0", "#1c4670", "#278ab0", "#1dc690"]
+    }, 
+    {
+        "liked": false,
+        "id": 3495,
+        "name": "Strawberry Froth",
+        "slug": "strawberry-froth",
+        "likes": 194,
+        "colors": ["#e43d40", "#f37970", "#f85c70", "#fabec0"]
+    }, 
+    {
+        "liked": false,
+        "id": 299,
+        "name": "Pale Blossom",
+        "slug": "pale-blossom",
+        "likes": 191,
+        "colors": ["#e1e2e7", "#fcc0c5", "#bf4c41", "#98042d"]
+    }, 
+    {
+        "liked": false,
+        "id": 412,
+        "name": "Crystal Feline",
+        "slug": "crystal-feline",
+        "likes": 181,
+        "colors": ["#5298c1", "#123175", "#c0c2c9", "#63747a"]
+    }, 
+    {
+        "liked": false,
+        "id": 1803,
+        "name": "Facing Forward",
+        "slug": "facing-forward",
+        "likes": 176,
+        "colors": ["#db1f48", "#004369", "#01949a", "#e5ddc8"]
+    }, 
+    {
+        "liked": false,
+        "id": 2748,
+        "name": "Thirst Quencher",
+        "slug": "thirst-quencher-3",
+        "likes": 174,
+        "colors": ["#fede00", "#b4f8c8", "#6ab8ee", "#a8d9f8"]
+    }
+]
+
+function generateSizing (min, max, count) {
+
+    function randombetween(min, max) {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+
+    var r = [];
+    var currsum = 0;
+    for(var i=0; i<count-1; i++) {
+       r[i] = randombetween(min, max-(count-i-1)-currsum);
+       currsum += r[i];
+    }
+    r[count-1] = max - currsum;
+    return r;
+}
+
+function shuffleArray (array) {
+
+    let length = array.length - 1
+
+    for (let i = length; i > 0; i--) {
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array;
+}
+  
+$(function () {
+
+    html  = ``
+
+    palettes.forEach(p => {
+
+        html += `<div class="row">`;
+        let shuffle = shuffleArray(p.colors);
+
+        for(let i=0; i < 4; i++) {
+            
+            let sizing = generateSizing(10, 100, 4)
+            html += '<div class="palette">';
+
+            
+            shuffle.forEach((c, i) => {
+                html += `<div class="panel" style="background-color:${c}; height:${sizing[i]}%"></div>`
+            });
+            
+            html += '</div>';
+
+        };
+
+        html += '</div>';
+
+    })
+
+    $('#gallery').append(html);
+
+    console.log(html)
+
+
+});
